4c040fb6a78b8353d456d9aefadf3b31e816b2b4
Ledy <ledycathell@outlook.com>
2025-08-23 08:52:37 -0400

    update5

--- controllers/clothingItems.js
+++ controllers/clothingItems.js
@@ -1,8 +1,59 @@
-const ClothingItem = require("../models/clothingItem");
+const Item = require("../models/clothingItem");
 const { BAD_REQUEST, NOT_FOUND, SERVER_ERROR } = require("../utils/errors");
 
+const getItems = (req, res) => {
+  Item.find({})
+    .then((items) => res.send(items))
+    .catch((err) => {
+      console.error(err);
+      return res
+        .status(SERVER_ERROR)
+        .send({ message: "An error has occurred on the server" });
+    });
+};
+
+const createItem = (req, res) => {
+  const { name, weather, imageUrl } = req.body;
+  const owner = req.user._id;
+
+  return Item.create({ name, weather, imageUrl, owner })
+    .then((item) => res.status(201).send(item))
+    .catch((err) => {
+      console.error(err);
+      if (err.name === "ValidationError") {
+        return res
+          .status(BAD_REQUEST)
+          .send({ message: "Invalid data passed when creating an item" });
+      }
+      return res
+        .status(SERVER_ERROR)
+        .send({ message: "An error has occurred on the server" });
+    });
+};
+
+const deleteItem = (req, res) => {
+  const { itemId } = req.params;
+
+  return Item.findByIdAndDelete(itemId)
+    .then((item) => {
+      if (!item) {
+        return res.status(NOT_FOUND).send({ message: "Item not found" });
+      }
+      return res.send({ message: "Item deleted", item });
+    })
+    .catch((err) => {
+      console.error(err);
+      if (err.name === "CastError") {
+        return res.status(BAD_REQUEST).send({ message: "Invalid item ID" });
+      }
+      return res
+        .status(SERVER_ERROR)
+        .send({ message: "An error has occurred on the server" });
+    });
+};
+
 const likeItem = (req, res) =>
-  ClothingItem.findByIdAndUpdate(
+  Item.findByIdAndUpdate(
     req.params.itemId,
     { $addToSet: { likes: req.user._id } },
     { new: true }
@@ -24,7 +75,7 @@ const likeItem = (req, res) =>
     });
 
 const dislikeItem = (req, res) =>
-  ClothingItem.findByIdAndUpdate(
+  Item.findByIdAndUpdate(
     req.params.itemId,
     { $pull: { likes: req.user._id } },
     { new: true }
@@ -45,4 +96,10 @@ const dislikeItem = (req, res) =>
         .send({ message: "An error has occurred on the server" });
     });
 
-module.exports = { likeItem, dislikeItem };
+module.exports = {
+  getItems,
+  createItem,
+  deleteItem,
+  likeItem,
+  dislikeItem,
+};
--- routes/clothingItems.js
+++ routes/clothingItems.js
@@ -2,63 +2,18 @@ const express = require("express");
 
 const router = express.Router();
 
-const Item = require("../models/clothingItem");
-const { BAD_REQUEST, NOT_FOUND, SERVER_ERROR } = require("../utils/errors");
-const { likeItem, dislikeItem } = require("../controllers/clothingItems");
+const {
+  getItems,
+  createItem,
+  deleteItem,
+  likeItem,
+  dislikeItem,
+} = require("../controllers/clothingItems");
 
-router.get("/", (req, res) =>
-  Item.find({})
-    .then((items) => res.send(items))
-    .catch((err) => {
-      console.error(err);
-      return res
-        .status(SERVER_ERROR)
-        .send({ message: "An error has occurred on the server" });
-    })
-);
-
-router.post("/", (req, res) => {
-  const { name, weather, imageUrl } = req.body;
-  const owner = req.user._id;
-
-  return Item.create({ name, weather, imageUrl, owner })
-    .then((item) => res.status(201).send(item))
-    .catch((err) => {
-      console.error(err);
-      if (err.name === "ValidationError") {
-        return res
-          .status(BAD_REQUEST)
-          .send({ message: "Invalid data passed when creating an item" });
-      }
-      return res
-        .status(SERVER_ERROR)
-        .send({ message: "An error has occurred on the server" });
-    });
-});
-
+router.get("/", getItems);
+router.post("/", createItem);
 router.put("/:itemId/likes", likeItem);
-
 router.delete("/:itemId/likes", dislikeItem);
-
-router.delete("/:itemId", (req, res) => {
-  const { itemId } = req.params;
-
-  return Item.findByIdAndDelete(itemId)
-    .then((item) => {
-      if (!item) {
-        return res.status(NOT_FOUND).send({ message: "Item not found" });
-      }
-      return res.send({ message: "Item deleted", item });
-    })
-    .catch((err) => {
-      console.error(err);
-      if (err.name === "CastError") {
-        return res.status(BAD_REQUEST).send({ message: "Invalid item ID" });
-      }
-      return res
-        .status(SERVER_ERROR)
-        .send({ message: "An error has occurred on the server" });
-    });
-});
+router.delete("/:itemId", deleteItem);
 
 module.exports = router;
