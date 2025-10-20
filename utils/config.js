8ce1c28a9085cb2f718d7efd3494a72f4a37577e
Ledy <ledycathell@outlook.com>
2025-10-20 18:39:12 -0400

    update

--- utils/config.js
+++ utils/config.js
@@ -1,4 +1,4 @@
-const { JWT_SECRET = "super-strong-secret" } = process.env;
+const { JWT_SECRET = "2e28f222d2980d0fc297920b86d7516d8aad016fd26a80564b88199cf193c8a8" } = process.env;
 
 module.exports = {
   JWT_SECRET,
